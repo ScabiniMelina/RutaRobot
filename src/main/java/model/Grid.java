fe980f34ec2608c4e7343e75a73c9425fe392f9a
bue-melinas <melina.scabini@gmail.com>
2025-05-25 10:04:03 -0300

    create base model clases

---
+++ src/main/java/model/Grid.java
@@ -0,0 +1,25 @@
+package model;
+
+import observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grid {
+    private Cell[][] grid;
+    private List<Observer> observers;
+
+    public Grid(Cell[][] grid, int rows, int columns, List<Observer> observers) {
+        this.grid = grid;
+        this.grid = new Cell[rows][columns];
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                grid[i][j] = new Cell(i, j, 1);
+            }
+        }
+
+        this.observers =  new ArrayList<Observer>();
+    }
+}
