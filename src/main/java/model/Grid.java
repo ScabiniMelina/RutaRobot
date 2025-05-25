4fb0c6b750be3166ccaf57bc666893f91e43aa3b
Mayra <mayra.rossi03@gmail.com>
2025-05-25 10:20:10 -0300

    Fix grid constructor

--- src/main/java/model/Grid.java
+++ src/main/java/model/Grid.java
@@ -9,17 +9,8 @@ public class Grid {
     private Cell[][] grid;
     private List<Observer> observers;
 
-    public Grid(Cell[][] grid, int rows, int columns, List<Observer> observers) {
+    public Grid(Cell[][] grid) {
         this.grid = grid;
-        this.grid = new Cell[rows][columns];
-
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                grid[i][j] = new Cell(i, j, 1);
-            }
-        }
-
         this.observers =  new ArrayList<Observer>();
     }
 }
